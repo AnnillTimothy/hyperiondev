99a08586d6698c01def9125bddc6c442788402ac
Annill <annillseale17@gmail.com>
2023-08-05 01:45:16 +0200

    commit updates

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,7 @@ tl.fromTo("nav", {opacity: 0}, {duration: 1, opacity: 1}, "-=.5");
 tl.fromTo(".hero", {opacity: 0}, {duration: 1, opacity: 1});
 tl.fromTo(".courses-section", {opacity: 0}, {duration: 1, opacity: 1}), "-=1";
 
+// Nav display on click
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -26,14 +27,57 @@ const navLinks = () => {
     });
 }
 
+// Hero Animations
+const triangle = document.querySelector('.triangle');
+        
+        // GSAP continuous animation
+        function animateTriangle() {
+            gsap.to(triangle, {
+                x: 20, // Target X position (end point)
+                y: 200, // Target Y position (end point)
+                duration: 2, // Animation duration in seconds
+                repeat: -1, // Infinite repetition (-1 for continuous)
+                yoyo: true, // Reverse animation on each repeat
+                ease: 'power1.inOut', // Easing function (optional)
+            });
+        }
+
+const circle = document.querySelector('.circle');
+    
+        function animateCircle() {
+            gsap.to(circle, {
+                x: -300, 
+                duration: 1, 
+                repeat: -1, 
+                yoyo: true, 
+                ease: 'power1.inOut', 
+            });
+        }
+
+const companyImg = document.querySelector('.company-ul');
+        
+        function animateCompany() {
+            gsap.to(companyImg, {
+                x: -300, 
+                duration: 2, 
+                repeat: -1, 
+                yoyo: true, 
+                ease: 'power1.inOut', 
+            });
+        }
+
 
 const app = () => {
     navSlide();
     navLinks();
+    animateTriangle();
+    animateCircle();
+    animateCompany();
 }
 
 app();
 
+//Checkbox that displays bootcamps
 const checkbox = document.getElementById('showHideCheckbox');
 const content1Div = document.getElementById('content1');
 const content2Div = document.getElementById('content2');
