44130a20f06c6f6292b65420eb2b006f28f68561
Annill <annillseale17@gmail.com>
2023-08-07 19:06:26 +0200

    sassy ententions added

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-// New GSAP timeline to handle how elements load
+// New GSAP timeline to handle when elements load
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", {y:"0%", duration: 1.5, stagger: 0.25 });
